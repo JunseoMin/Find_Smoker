a6591010a8ce4c66f033bdaa9b47faec71b4a5a0
JunsoMin <minjs4562@gmail.com>
2023-08-14 17:02:05 +0900

    model.py module init

--- src/model.py
+++ src/model.py
@@ -2,3 +2,14 @@ import torch
 import pandas as pd
 import numpy as np
 
+class ResNet:
+    def __init__(self) -> None:
+        pass
+
+class KernelSVM:
+    def __init__(self) -> None:
+        pass
+
+class LightGBM:
+    def __init__(self) -> None:
+        pass
